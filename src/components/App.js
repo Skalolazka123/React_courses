584f9ebab4ec7378457e79e59c59ed621f14dbbf
Skalolazka123 <skalolazka123@gmail.com>
2018-02-09 21:12:13 +0300

    First 7 video

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Article from "./Article.js"
+import ArticleList from "./ArticleList.js"
 import articles from "../fixtures"
 
 function App() {
     return (
         < div >
         < h1 > App Name < /h1>
-        < Article article={articles[1]}/ >
+        < ArticleList articles={articles}/ >
         < /div>
     )
 }
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,66 @@
+import React, {Component} from 'react'
+
+class Article extends Component {
+    state = {
+        isOpen:false
+    }
+    /*
+    *Аналог (beta-запись)
+    constructor(props){
+        super(props)
+        this.state = {
+            isOpen: true
+        }
+    }
+    */
+    render() {
+        const {article} = this.props
+        console.log('===', this.props)
+        const body = this.state.isOpen && < section > {article.text} < /section>
+        return (
+            < div >
+                < h2 > {article.title}
+                    < button onClick = {this.handleClick} > {this.state.isOpen ? 'close' : 'open'} < /button>
+                < /h2>
+                {body}
+                < h3 >
+                    creation date: {(new Date(article.date)).toDateString()}
+                < /h3>
+            < /div>
+        )
+    }
+    handleClick = () => {
+        console.log('===','clicked')
+        //Метод, где описывается состояние
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+/*
+*Stateless component
+*/
+/*
+
 import React from 'react'
 
+
 function Article(props) {
-    const {article} = props
-    const body = < section >{article.text}< /section>
-    return ( < div>
-        < h2 > {article.title} < /h2> {
-            body
-        } < h3 >
-        creation date: {
-            (new Date(article.date)).toDateString()
-        } < /h3> < /div>
-    )
+        const {article} = props
+        const body = < section > {article.text} < /section>
+        return (
+            < div >
+                < h2 > {article.title}
+                    < button onClick = {handleClick} > close < /button>
+                < /h2>
+                {body}
+                < h3 >
+                    creation date: {(new Date(article.date)).toDateString()}
+                < /h3>
+            < /div>
+        )
 }
+*/
+
 
 export default Article
